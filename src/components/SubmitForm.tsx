c753a4eba1542b274b5569110e6dcb3720dd0e80
test <grjochem2@gmail.com>
2025-12-18 17:11:22 +0100

    small fix

--- src/components/SubmitForm.tsx
+++ src/components/SubmitForm.tsx
@@ -57,6 +57,7 @@ export default function MyForm() {
   });
 
   const onSubmit = (data: InputType) => {
+    setState('submitting');
     fetch('https://aap.gurlslife.com/upload', {
       method: 'POST',
       headers: {
